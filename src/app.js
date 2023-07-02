5b8f7d8f2c0ba7c7bf6dcd3a71e7f64f07d1f401
mr_sd_jack_003 <barson0habra@gmail.com>
2023-07-03 00:14:16 +0530

    normal update

--- src/app.js
+++ src/app.js
@@ -91,9 +91,12 @@ const createDocument = async () =>{
 //read data in this process
 const getDocument = async () => {
    try{
-      const result = await Playlist.find({ctype: "Front End"})
-   .select({name: 1})
-   .limit(1);//specifie here using  the type of the element you want also set here the limit you want to show the data
+      const result = await Playlist
+      // .find({ctype: "Front End"})
+      .find({ctype: {$in: ["Back End"]}})
+      //.find({videos:{$gt: 20}}) //inbuild fn '$' ----> $gt(greater than), $gte(greater than equal), $lt(less than), $lte(less than equal) THIS ALL ARE THE INBUILD FUNCTION.
+      .select({name: 1})
+      //.limit(1);//specifie here using  the type of the element you want also set here the limit you want to show the data
    console.log(result);
    }catch(err){
       console.log(err);
